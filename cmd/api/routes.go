4493c35123618e5413ab0c942440c4a7a74b1290
pernebeksa <pernebeksa@kolesa.kz>
2025-07-26 18:28:24 +0500

    tasks model was added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,7 +14,7 @@ func (app *application) routes() http.Handler {
 		v1.GET("/tasks", app.getAllTasks)
 		v1.GET("/tasks/:id", app.getTask)
 		v1.PUT("/tasks/:id", app.updateTask)
-		v1.DELETE("events/:id", app.deleteEvent)
+		v1.DELETE("events/:id", app.deleteTask)
 	}
 
 	return g
