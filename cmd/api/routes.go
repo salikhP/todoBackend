fe90e45f4d1f125bb8d977a97de28169ed0c5a1d
salikhP <pernebek.sm@gmail.com>
2025-08-31 10:57:29 +0500

    feat: protect routes by auth middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,23 +9,30 @@ func (app *application) routes() http.Handler {
 	g := gin.Default()
 
 	v1 := g.Group("/api/v1")
+
+	// public routes
 	{
-		v1.POST("/tasks", app.createTask)
-		v1.GET("/tasks", app.getAllTasks)
-		v1.GET("/tasks/:id", app.getTask)
-		v1.PUT("/tasks/:id", app.updateTask)
-		v1.DELETE("tasks/:id", app.deleteTask)
-
-		v1.POST("/taskTypes", app.createTaskType)
-		v1.GET("/taskTypes", app.getAllTaskTypes)
-		v1.GET("/taskTypes/:id", app.getTaskType)
-		v1.PUT("/taskTypes/:id", app.updateTaskType)
-		v1.DELETE("/taskTypes/:id", app.deleteTaskType)
-
-		v1.GET("/users/:userId/tasks", app.listTasks)
-
 		v1.POST("/auth/register", app.registerUser)
 		v1.POST("/auth/login", app.login)
+	}
+
+	// protected routes (with AuthMiddleware)
+	authGroup := v1.Group("/")
+	authGroup.Use(app.AuthMiddleware())
+	{
+		authGroup.POST("/tasks", app.createTask)
+		authGroup.GET("/tasks", app.getAllTasks)
+		authGroup.GET("/tasks/:id", app.getTask)
+		authGroup.PUT("/tasks/:id", app.updateTask)
+		authGroup.DELETE("tasks/:id", app.deleteTask)
+
+		authGroup.POST("/taskTypes", app.createTaskType)
+		authGroup.GET("/taskTypes", app.getAllTaskTypes)
+		authGroup.GET("/taskTypes/:id", app.getTaskType)
+		authGroup.PUT("/taskTypes/:id", app.updateTaskType)
+		authGroup.DELETE("/taskTypes/:id", app.deleteTaskType)
+
+		authGroup.GET("/users/:userId/tasks", app.listTasks)
 	}
 
 	return g
